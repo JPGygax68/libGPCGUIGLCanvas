925499e6fc3a980c9ad974a541cd45e31443b972
Jean-Pierre Gygax <gygax@practicomp.ch>
2014-12-20 21:43:24 +0100

    Adding external dependency GPC GUICanvas as a Git submodule.

--- src/painter.cpp
+++ src/painter.cpp
@@ -1,34 +1,9 @@
-#include <iostream>
-#include <thread>
-#include <mutex>
-#include <string>
-
-#include <GL/glew.h>
-
-#include <gpc/gl/wrappers.hpp>
-
 #include <gpc/gui/gl/painter.hpp>
-
-static char vertex_shader[] = {
-#include <vertex.glsl.h>
-};
 
 namespace gpc { namespace gui { namespace gl {
 
-    Painter::Painter():
-        vertex_shader(0), fragment_shader(0)
-    {
-    }
-
-    void Painter::init()
-    {
-        static std::once_flag flag;
-        std::call_once(flag, []() { glewInit(); } );
-
-        // Upload and compile our shader program
-        vertex_shader = CALL_GL(glCreateShader, GL_FRAGMENT_SHADER);
-
-        // TODO...
-    }
+    std::string Painter::vertex_code {
+        #include "vertex.glsl.h"
+    };
 
 } } } // gpc::gui::gl
--- testapp/main.cpp
+++ testapp/main.cpp
@@ -1,4 +1,7 @@
+#include <cassert>
+#include <cstdio>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 //#include <SDL2/SDL_opengl.h>
 #include <iostream>
 using std::cout;
@@ -7,63 +10,75 @@ using std::cout;
 
 int main(int argc, char *argv[])
 {
-    int width = 640, height = 480;
+    try {
 
-    SDL_Init(SDL_INIT_VIDEO);
+        int width = 640, height = 480;
+
+        SDL_Init(SDL_INIT_VIDEO);
+        IMG_Init(IMG_INIT_PNG);
     
-    SDL_Window *window = SDL_CreateWindow("Graphics Application", 
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-        width, height, 
-        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-    SDL_GLContext glcontext = SDL_GL_CreateContext(window);
-    cout << "OpenGL Version " << glGetString(GL_VERSION) << "\n";
+        auto img_surf = IMG_Load("../../assets/uniform_fill.png");
 
-    auto adaptToWindowDimensions = [](unsigned int width, unsigned int height) {
-        glViewport(0, 0, width, height);
-        glLoadIdentity();
-        glFrustum(-1, 1, -(float)height / width, (float)height / width, 1, 500);
-    };
+        SDL_Rect disp_bounds;
+        SDL_GetDisplayBounds(0, &disp_bounds);
+        SDL_Window *window = SDL_CreateWindow("Graphics Application",
+            0, 0, disp_bounds.w, disp_bounds.h,
+            SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS);
+        SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+        cout << "OpenGL Version " << glGetString(GL_VERSION) << "\n";
+
+        auto context = SDL_GL_CreateContext(window);
+
+        glewInit();
+
+        auto adaptToWindowDimensions = [](unsigned int width, unsigned int height) {
+            glViewport(0, 0, width, height);
+            glLoadIdentity();
+            glFrustum(-1, 1, -(float)height / width, (float)height / width, 1, 500);
+        };
     
-    glClearColor(0, 0, 0, 1);
+        glClearColor(0, 0, 0, 1);
 
-    adaptToWindowDimensions(width, height);
+        adaptToWindowDimensions(width, height);
 
-    gpc::gui::gl::Painter painter;
+        gpc::gui::gl::Painter painter;
 
-    painter.init();
+        painter.init();
 
-    SDL_Event event;
-    while (1)
-    {
-        while (SDL_PollEvent(&event))
+        SDL_Event event;
+        while (1)
         {
-            switch (event.type)
+            while (SDL_PollEvent(&event))
             {
-            case SDL_WINDOWEVENT:
-                if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
-                    adaptToWindowDimensions(event.window.data1, event.window.data2);
-                }
-                break;
+                switch (event.type)
+                {
+                case SDL_WINDOWEVENT:
+                    if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+                        adaptToWindowDimensions(event.window.data1, event.window.data2);
+                    }
+                    break;
 
-            case SDL_QUIT: 
-                SDL_GL_DeleteContext(glcontext); 
-                SDL_DestroyWindow(window); 
-                SDL_Quit(); 
-                return 0;
+                case SDL_QUIT: 
+                    SDL_GL_DeleteContext(glcontext); 
+                    SDL_DestroyWindow(window); 
+                    SDL_Quit(); 
+                    return 0;
+                }
             }
+
+            painter.fill_rect(10, 10, 300, 150, painter.rgb_to_native({1, 1, 1}) );
+
+            SDL_GL_SwapWindow(window);
+
+            SDL_Delay(20);
         }
 
-        static float delta = 0;
-        delta += 0.002f;
-        
-        glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glVertex3f(delta, 0, -5);
-        glVertex3f(1 + delta, 0, -5);
-        glVertex3f(delta, 1, -5);
-        glEnd();
-        
-        SDL_GL_SwapWindow(window);
-        SDL_Delay(20);
+        return 0;
     }
+    catch(const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    catch(...) {}
+
+    return 1;
 }
