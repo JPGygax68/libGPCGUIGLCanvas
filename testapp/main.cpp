267d43ef59c71ed78f05bd828c923b7efa069771
Jean-Pierre Gygax <gygax@practicomp.ch>
2014-12-30 22:07:42 +0100

    Working towards making the Canvas Test suite work with the OpenGL implementation

--- testapp/main.cpp
+++ testapp/main.cpp
@@ -7,9 +7,6 @@
 //#include <SDL2/SDL_opengl.h>
 #include <gpc/gl/wrappers.hpp>
 #include <gpc/gui/gl/canvas.hpp>
-#include <gpc/fonts/RasterizedFont.hpp>
-#include <gpc/fonts/cereal.hpp>
-#include <cereal/archives/binary.hpp>
 
 #include <gpc/gui/canvas_testsuite.hpp>
 
@@ -18,76 +15,77 @@ using gpc::gui::RGBAFloat;
 using gpc::gui::RGBA32;
 using gpc::gui::gl::Canvas;
 
-static char liberations_sans_data[] = {
-#include "LiberationSans-Regular-20.rft.h"
-};
-
-static auto
-makeColorInterpolatedRectangle(size_t width, size_t height, const std::array<RGBAFloat,4> &corner_colors) -> std::vector<RGBA32>
-{
-    std::vector<RGBA32> image(width * height);
-
-    auto it = image.begin();
-    for (auto y = 0U; y < height; y ++) {
-        for (auto x = 0U; x < width; x ++) {
-            RGBAFloat top    = interpolate(corner_colors[0], corner_colors[1], float(x) / float(width));
-            RGBAFloat bottom = interpolate(corner_colors[2], corner_colors[3], float(x) / float(width));
-            RGBAFloat color  = interpolate(top, bottom, float(y) / float(height));
-            *it = fromFloat(color);
-            //*it = { {255, 0, 0, 255} };
-            it++;
-        }
-    }
-
-    return image;
-}
-
 int main(int argc, char *argv[])
 {
+    typedef gpc::gui::gl::Canvas<true> canvas_t;
+    typedef std::pair<SDL_Window*, SDL_GLContext> display_t;
+    typedef gpc::gui::CanvasTestsuite<canvas_t, display_t> MyCanvasTestsuite;
+    typedef MyCanvasTestsuite::context_t context_t;
+
+    typedef MyCanvasTestsuite::draw_fn_t draw_fn_t;
+
     try {
 
-        int width = 640, height = 480;
+        //int width = 640, height = 480;
 
-        std::string data_string(liberations_sans_data, liberations_sans_data + sizeof(liberations_sans_data));
-        std::stringstream sstr(data_string);
-        cereal::BinaryInputArchive ar(sstr);
-        gpc::fonts::RasterizedFont rfont;
-        ar >> rfont;
-        
         SDL_Init(SDL_INIT_VIDEO);
         IMG_Init(IMG_INIT_PNG);
     
-        SDL_Rect disp_bounds;
-        SDL_GetDisplayBounds(0, &disp_bounds);
-        SDL_Window *window = SDL_CreateWindow("Graphics Application",
-            0, 0, disp_bounds.w, disp_bounds.h,
-            SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS);
-        SDL_GLContext glcontext = SDL_GL_CreateContext(window);
-        cout << "OpenGL Version " << glGetString(GL_VERSION) << "\n";
+        SDL_Rect main_display_bounds;
+        SDL_GetDisplayBounds(0, &main_display_bounds);
 
-        SDL_GetWindowSize(window, &width, &height);
+        //auto adaptToWindowDimensions = [&](unsigned int width, unsigned int height) {
+        //    canvas.define_viewport(0, 0, width, height);
+        //};
 
-        auto context = SDL_GL_CreateContext(window);
+        draw_fn_t draw_fn;
 
-        glewInit();
+        auto draw_window = [&](display_t display, canvas_t &canvas) -> void {
 
-        EXEC_GL(glClearColor, 0.0f, 0.0f, 0.0f, 1.0f);
-
-        typedef Canvas<true> MyCanvas;
-        MyCanvas canvas;
-
-        canvas.init();
-
-        auto adaptToWindowDimensions = [&](unsigned int width, unsigned int height) {
-            canvas.define_viewport(0, 0, width, height);
+            canvas.prepare_context();
+            draw_fn(display, canvas);
+            SDL_GL_SwapWindow(display.first);
+            canvas.leave_context();
         };
 
-        adaptToWindowDimensions(width, height);
+        auto create_window = [&](int w, int h, draw_fn_t draw_fn_) -> context_t {
 
-        auto test_image = makeColorInterpolatedRectangle(170, 130, {{ {1, 0, 0, 1}, {0, 1, 0, 1}, {0, 0, 1, 1}, {1, 1, 1, 1}} });
-        auto test_image_handle = canvas.register_rgba_image(170, 130, &test_image[0]);
-        auto my_font = canvas.register_font(rfont);
+            Uint32 flags = SDL_WINDOW_OPENGL;
+            if (w == 0 && h == 0) { 
+                w = main_display_bounds.w, h = main_display_bounds.h; 
+                flags |= SDL_WINDOW_BORDERLESS;
+            }
+            SDL_Window *window = SDL_CreateWindow("GPC GUI OpenGL Canvas test window", 0, 0, w, h, flags);
+            SDL_GLContext gl_ctx = SDL_GL_CreateContext(window);
+            glewInit();
+            cout << "OpenGL Version " << glGetString(GL_VERSION) << "\n";
+            EXEC_GL(glClearColor, 0.0f, 0.0f, 0.0f, 1.0f);
 
+            int wr, hr;
+            SDL_GetWindowSize(window, &wr, &hr);
+            assert(wr == w && hr == h); // TODO: return the actual dimension to have test suite check?
+
+            draw_fn = draw_fn_;
+            
+            canvas_t *canvas = new canvas_t();
+            display_t disp = std::make_pair(window, gl_ctx);
+
+            draw_window(disp, *canvas);
+
+            return std::make_pair(disp, canvas);
+        };
+
+        auto destroy_window = [&](display_t disp) -> void {
+
+            SDL_GL_DeleteContext(disp.second);
+            SDL_DestroyWindow(disp.first);
+        };
+
+        MyCanvasTestsuite testsuite(create_window, destroy_window);
+
+        testsuite.run_all_tests();
+
+        /*
         SDL_Event event;
         while (1)
         {
@@ -97,66 +95,20 @@ int main(int argc, char *argv[])
                 {
                 case SDL_WINDOWEVENT:
                     if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
-                        adaptToWindowDimensions(event.window.data1, event.window.data2);
+                        testsuite.update_canvas_size(event.window.data1, event.window.data2);
+                        // TODO: adapt to window dimensions adaptToWindowDimensions(event.window.data1, event.window.data2);
                     }
                     break;
 
                 case SDL_QUIT: 
-                    SDL_GL_DeleteContext(glcontext); 
-                    SDL_DestroyWindow(window); 
                     SDL_Quit(); 
                     return 0;
                 }
             }
 
-            static const int SEPARATION = 20;
-            int x = 50, y = 50, w, h;
-
-            canvas.prepare_context();
-
-            // TODO: use GPC layout module ?
-
-            canvas.fill_rect(50, y, 150, 150, canvas.rgb_to_native({ 1, 0, 0 }));
-            canvas.fill_rect(50 + 150 + 10, y, 150, 150, canvas.rgb_to_native({ 0, 1, 0 }));
-            y += 150 + 10;
-            canvas.fill_rect(50, y, 150, 150, canvas.rgb_to_native({ 0, 0, 1 }));
-            canvas.fill_rect(50 + 150 + 10, y, 150, 150, canvas.rgb_to_native({ 1, 1, 1 }));
-            
-            x += 150 + 10 + 150 + SEPARATION, y = 50;
-            // Single image
-            canvas.draw_image(x, y, 170, 130, test_image_handle);
-            x += 170 + SEPARATION;
-            // Repeated image
-            w = 2 * 170 + 8, h = 2 * 130 + 5;
-            canvas.draw_image(x, y, w, h, test_image_handle);
-            // Repeated, with clipping
-            x += w + SEPARATION;
-            canvas.set_clipping_rect(x + 20, y + 20, w - 40, h - 40);
-            canvas.draw_image(x, y, w, h, test_image_handle);
-            canvas.cancel_clipping();
-            // Image with offset
-            x += w + 20;
-            canvas.draw_image(x, y, w, h, test_image_handle, 20, 20);
-            y += 310;
-
-            // Some text
-            x = 50; y += 20;
-            // Ascent (estimated) = 15; TODO: correct for top-down, but for bottom-up, descent should be used
-            canvas.draw_text(my_font, x, y+15, "ABCDEFabcdef,;", 14);
-            canvas.fill_rect(x, y+15 - 1, x + 150, 1, canvas.rgba_to_native({ 1, 0, 0, 0.5f }));
-            y += 20;
-            // With clipping
-            y += 10;
-            canvas.set_clipping_rect(x + 5, y+3, 100, 20 - 3 - 3);
-            canvas.draw_text(my_font, x, y + 15, "Clipping clipping clipping", 26);
-            canvas.cancel_clipping();
-
-            canvas.leave_context();
-
-            SDL_GL_SwapWindow(window);
-
             SDL_Delay(20);
         }
+        */
 
         return 0;
     }
