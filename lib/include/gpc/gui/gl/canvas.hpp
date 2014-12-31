306391a4ca7ca89f4a2c340f2e6cbe163b21ed88
Jean-Pierre Gygax <gygax@practicomp.ch>
2014-12-31 13:04:11 +0100

    Progressing but still more work needed

--- lib/include/gpc/gui/gl/canvas.hpp
+++ lib/include/gpc/gui/gl/canvas.hpp
@@ -11,6 +11,7 @@
 #endif
 #include <GL/glew.h>
 
+#include <gpc/gui/canvas.hpp>
 #include <gpc/gl/wrappers.hpp>
 #include <gpc/gl/utils.hpp>
 #include <gpc/fonts/RasterizedFont.hpp>
@@ -18,56 +19,6 @@
 namespace gpc {
 
     namespace gui {
-
-        // TODO: RGBFloat and RGBAFloat are implementation-independent and belong in
-        // the module defining the Canvas concept
-
-        struct RGBFloat {
-            GLfloat r, g, b;
-        };
-
-        struct RGBAFloat {
-            GLfloat r, g, b, a;
-            RGBAFloat(): RGBAFloat { 0, 0, 0, 1 } {}
-            RGBAFloat(GLfloat r_, GLfloat g_, GLfloat b_, GLfloat a_ = 1): r(r_), g(g_), b(b_), a(a_) {}
-            auto operator / (float dsor) -> RGBAFloat & {
-                r /= dsor, g /= dsor, b /= dsor, a /= dsor;
-                return *this;
-            }
-        };
-
-        inline auto 
-        operator + (const RGBAFloat &color1, const RGBAFloat &color2) -> RGBAFloat 
-        {
-            return { color1.r + color2.r, color1.g + color2.g, color1.b + color2.b, color1.a + color2.a };
-        }
-
-        inline auto
-        interpolate(const RGBAFloat &color1, const RGBAFloat &color2, float a) -> RGBAFloat 
-        {
-            RGBAFloat result;
-            result.r = color1.r + a * (color2.r - color1.r);
-            result.g = color1.g + a * (color2.g - color1.g);
-            result.b = color1.b + a * (color2.b - color1.b);
-            result.a = color1.a + a * (color2.a - color1.a);
-            return result;
-        }
-
-        struct RGBA32 {
-            uint8_t components[4];
-        };
-
-        inline auto 
-        fromFloat(const RGBAFloat &from) -> RGBA32 
-        {
-            return { { uint8_t(from.r * 255), uint8_t(from.g * 255), uint8_t(from.b * 255), uint8_t(from.a * 255) } };
-        }
-
-        // TODO: use a struct from GPC Fonts ?
-        struct GlyphHeight { 
-            // TODO: use integer types with well-defined sizes
-            int ascent, descent; 
-        };
 
         namespace gl {
 
--- testapp/main.cpp
+++ testapp/main.cpp
@@ -11,104 +11,142 @@
 #include <gpc/gui/canvas_testsuite.hpp>
 
 using std::cout;
-using gpc::gui::RGBAFloat;
-using gpc::gui::RGBA32;
-using gpc::gui::gl::Canvas;
+
+struct Harness {
+
+    typedef gpc::gui::gl::Canvas<true>                              canvas_t;
+    typedef std::pair<SDL_Window*, SDL_GLContext>                   display_t;
+    typedef gpc::gui::CanvasTestSuite<canvas_t, display_t, Harness> TestSuite;
+    typedef TestSuite::init_fn_t                                    init_fn_t;
+    typedef TestSuite::cleanup_fn_t                                 cleanup_fn_t;
+    typedef TestSuite::draw_fn_t                                    draw_fn_t;
+    typedef TestSuite::context_t                                    context_t;
+
+    Harness(): window(nullptr), gl_ctx(0), canvas(nullptr) {}
+
+    void init() 
+    {
+        SDL_GetDisplayBounds(0, &main_display_bounds);
+    }
+
+    auto create_window(int w, int h, init_fn_t init_fn, draw_fn_t draw_fn) 
+        -> context_t 
+    {
+        if (window) throw std::runtime_error("This test harness can only open one window/display at a time.");
+        assert(gl_ctx == 0);
+
+        Uint32 flags = SDL_WINDOW_OPENGL;
+        if (w == 0 && h == 0) {
+            w = main_display_bounds.w, h = main_display_bounds.h;
+            flags |= SDL_WINDOW_HIDDEN | SDL_WINDOW_BORDERLESS;
+        }
+        window = SDL_CreateWindow("GPC GUI OpenGL Canvas test window", 0, 0, w, h, flags);
+        gl_ctx = SDL_GL_CreateContext(window);
+        glewInit();
+        cout << "OpenGL Version " << glGetString(GL_VERSION) << "\n";
+        EXEC_GL(glClearColor, 0.0f, 0.0f, 0.0f, 1.0f);
+
+        int wr, hr;
+        SDL_GetWindowSize(window, &wr, &hr);
+        assert(wr == w && hr == h); // TODO: return the actual dimension to have test suite check?
+
+        canvas_t *canvas = new canvas_t();
+        display_t disp = std::make_pair(window, gl_ctx);
+
+        if (init_fn) init_fn(disp, canvas);
+
+        draw_fn(disp, canvas);
+
+        return std::make_pair(disp, canvas);
+    }
+
+    void destroy_window(display_t disp, cleanup_fn_t cleanup_fn) 
+    {
+        assert(canvas);
+
+        if (cleanup_fn) cleanup_fn(disp, canvas);
+
+        delete canvas;
+        canvas = nullptr;
+
+        assert(disp.second == gl_ctx);
+        SDL_GL_DeleteContext(disp.second);
+        gl_ctx = 0;
+
+        assert(disp.first == window);
+        SDL_DestroyWindow(disp.first);
+        window = 0;
+    }
+
+    void present_window(display_t display, draw_fn_t draw_fn)
+    {
+        assert(display.first == window);
+        SDL_ShowWindow(display.first);
+
+        bool running = true;
+        while (running)
+        {
+            SDL_Event event;
+            while (SDL_WaitEvent(&event))
+            {
+                switch (event.type)
+                {
+                case SDL_WINDOWEVENT:
+                    if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+                        //test_suite.update_canvas_size(event.window.data1, event.window.data2);
+                        // TODO: adapt to window dimensions adaptToWindowDimensions(event.window.data1, event.window.data2);
+                    }
+                    else if (event.window.event == SDL_WINDOWEVENT_EXPOSED) {
+                        render_content(display, canvas, draw_fn);
+                    }
+                    else if (event.window.event == SDL_WINDOWEVENT_CLOSE) {
+                        running = false;
+                    }
+                    break;
+
+                case SDL_QUIT:
+                    SDL_Quit();
+                    running = false;
+                    return;
+                }
+            }
+
+            //SDL_Delay(20);
+        }
+    }
+
+    void render_content(display_t display, canvas_t *canvas, draw_fn_t draw_fn)
+    {
+        canvas->prepare_context();
+        draw_fn(display, canvas);
+        SDL_GL_SwapWindow(display.first);
+        canvas->leave_context();
+    };
+
+private:
+
+    SDL_Rect            main_display_bounds;
+    SDL_Window          *window;
+    SDL_GLContext       gl_ctx;
+    canvas_t            *canvas;
+};
 
 int main(int argc, char *argv[])
 {
-    typedef gpc::gui::gl::Canvas<true> canvas_t;
-    typedef std::pair<SDL_Window*, SDL_GLContext> display_t;
-    typedef gpc::gui::CanvasTestsuite<canvas_t, display_t> MyCanvasTestsuite;
-    typedef MyCanvasTestsuite::context_t context_t;
-
-    typedef MyCanvasTestsuite::draw_fn_t draw_fn_t;
-
     try {
 
         //int width = 640, height = 480;
 
         SDL_Init(SDL_INIT_VIDEO);
         IMG_Init(IMG_INIT_PNG);
-    
-        SDL_Rect main_display_bounds;
-        SDL_GetDisplayBounds(0, &main_display_bounds);
+
+        Harness::TestSuite test_suite;
 
         //auto adaptToWindowDimensions = [&](unsigned int width, unsigned int height) {
         //    canvas.define_viewport(0, 0, width, height);
         //};
 
-        draw_fn_t draw_fn;
-
-        auto draw_window = [&](display_t display, canvas_t &canvas) -> void {
-
-            canvas.prepare_context();
-            draw_fn(display, canvas);
-            SDL_GL_SwapWindow(display.first);
-            canvas.leave_context();
-        };
-
-        auto create_window = [&](int w, int h, draw_fn_t draw_fn_) -> context_t {
-
-            Uint32 flags = SDL_WINDOW_OPENGL;
-            if (w == 0 && h == 0) { 
-                w = main_display_bounds.w, h = main_display_bounds.h; 
-                flags |= SDL_WINDOW_BORDERLESS;
-            }
-            SDL_Window *window = SDL_CreateWindow("GPC GUI OpenGL Canvas test window", 0, 0, w, h, flags);
-            SDL_GLContext gl_ctx = SDL_GL_CreateContext(window);
-            glewInit();
-            cout << "OpenGL Version " << glGetString(GL_VERSION) << "\n";
-            EXEC_GL(glClearColor, 0.0f, 0.0f, 0.0f, 1.0f);
-
-            int wr, hr;
-            SDL_GetWindowSize(window, &wr, &hr);
-            assert(wr == w && hr == h); // TODO: return the actual dimension to have test suite check?
-
-            draw_fn = draw_fn_;
-            
-            canvas_t *canvas = new canvas_t();
-            display_t disp = std::make_pair(window, gl_ctx);
-
-            draw_window(disp, *canvas);
-
-            return std::make_pair(disp, canvas);
-        };
-
-        auto destroy_window = [&](display_t disp) -> void {
-
-            SDL_GL_DeleteContext(disp.second);
-            SDL_DestroyWindow(disp.first);
-        };
-
-        MyCanvasTestsuite testsuite(create_window, destroy_window);
-
-        testsuite.run_all_tests();
-
-        /*
-        SDL_Event event;
-        while (1)
-        {
-            while (SDL_PollEvent(&event))
-            {
-                switch (event.type)
-                {
-                case SDL_WINDOWEVENT:
-                    if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
-                        testsuite.update_canvas_size(event.window.data1, event.window.data2);
-                        // TODO: adapt to window dimensions adaptToWindowDimensions(event.window.data1, event.window.data2);
-                    }
-                    break;
-
-                case SDL_QUIT: 
-                    SDL_Quit(); 
-                    return 0;
-                }
-            }
-
-            SDL_Delay(20);
-        }
-        */
+        test_suite.run_all_tests();
 
         return 0;
     }
